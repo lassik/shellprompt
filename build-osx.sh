349a71e3f286c5d9d8924e23aaca58d7a02130d1
Lassi Kortela <lassi@lassikortela.net>
2014-12-04 21:52:03 +0200

    Don't delete build directory since it may be the working directory we're running from and it confuses shells if their wd is deleted... just delete the contents.

--- build-osx.sh
+++ build-osx.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 set -e
 cd "$(dirname "$0")"
-test -e build-osx && rm -rf build-osx
-mkdir -m 0700 build-osx
+test -d build-osx || mkdir -m 0700 build-osx
+find build-osx -mindepth 1 -delete
 cd build-osx
 luac -o shellprompt.luac ../shellprompt.lua
 lua ../file2h.lua shellprompt_lua shellprompt.luac > shellprompt_lua.h
