239a50fe99633a0f4172947e1312f06147dd3e29
Lassi Kortela <lassi@lassikortela.net>
2014-12-16 23:35:12 +0200

    Implement 'set' command that saves program (rest of command line args) into XDG config directory. Right now there's no way to actually run the saved program to produce the prompt.

--- shellprompt.lua
+++ shellprompt.lua
@@ -1,3 +1,4 @@
+local PROGNAME  = "shellprompt"
 local last_ansi = nil
 local is_bash = false
 local is_csh  = false
@@ -5,6 +6,11 @@ local is_zsh  = false
 local buffer  = ""
 
 -- Utilities
+
+function die(msg)
+  io.stderr:write(PROGNAME..": "..msg.."\n")
+  os.exit(1)
+end
 
 function consumer(ary)
   local i = 1
@@ -25,6 +31,54 @@ end
 function rfind(s, ch)
   local i = s:reverse():find(ch, 1, true)
   if not i then return nil else return #s - i + 1 end
+end
+
+function map(tabl, fn)
+  ans = {}
+  for _, item in ipairs(tabl) do
+    table.insert(ans, fn(item))
+  end
+  return ans
+end
+
+function try_insert_conf_dir(tabl, envar, suffix)
+  local dir = os.getenv(envar)
+  if not (dir and dir:match("^/")) then return end
+  dir = dir.."/"..(suffix or "").."/"..PROGNAME
+  dir = dir:gsub("/+", "/"):gsub("/$", "").."/"
+  table.insert(tabl, dir)
+end
+
+function get_xdg_config_homes()
+  local homes = {}
+  try_insert_conf_dir(homes, "XDG_CONFIG_HOME")
+  try_insert_conf_dir(homes, "HOME", ".config")
+  return homes
+end
+
+function get_program_dirname_for_writing()
+  local homes = get_xdg_config_homes()
+  -- for i, home in ipairs(homes) do print(i, home) end
+  if #homes == 0 then die("Home directory not found") end
+  local home = homes[1]
+  shellprompt_os.ensure_dir_exists(home)
+  return home
+end
+
+function save_program(argiter, filename)
+  local stream = assert(io.open(filename, "w"))
+  local written = false
+  for arg in argiter do
+    if written then stream:write(" ") end
+    if string.len(arg) == 0 or string.find(arg, "[\"\\\\' ]") then
+      stream:write('"'..string.gsub(arg, "([\"\\\\])", "\\%1")..'"')
+    else
+      stream:write(arg)
+    end
+    written = true
+  end
+  if written then stream:write("\n") end
+  stream:close()
 end
 
 -- Buffer
@@ -143,29 +197,28 @@ local commands = {
 
 }
 
+-- Actions
+
+function set_action(nextarg)
+  save_program(nextarg, get_program_dirname_for_writing().."prompt")
+end
+
+local actions = {
+  set = set_action,
+}
+
 -- Main
 
 function main()
   local nextarg = consumer(arg)
-  for a in nextarg do
-    if a == "text" then
-      local v = nextarg()
-      if not v then
-        die2("text", "command requies an argument")
-        exit()
-      end
-      put(v)
-    else
-      local v = commands[a]
-      if not v then
-        print("no such command: "..a)
-        os.exit(1)
-      end
-      v()
-    end
+  local actionname = nextarg()
+  local action = actions[actionname]
+  if not actionname then
+    die("usage")
+  elseif not action then
+    die("unknown action: "..actionname)
   end
-  commands["reset"]()
-  io.write(buffer, "\n")
+  action(nextarg)
 end
 
 main()
