dd5aaf5ec14e2e38dd67e9ae8a4ec9b33612b9f9
Lassi Kortela <lassi@lassikortela.net>
2014-12-19 14:14:52 +0200

    Allow user to pass custom LUA_CFLAGS, LUA_LDFLAGS and LUA_PATH in build-config.sh (in case lua is not available from pkg-config in the usual manner)

--- build-linux.sh
+++ build-linux.sh
@@ -1,11 +1,25 @@
 #! /bin/sh
 set -e
 cd "$(dirname "$0")"
+if test -f ./build-config.sh ; then
+    echo "Using custom build options from build-config.sh"
+    . ./build-config.sh
+else
+    echo "Using default build options (build-config.sh not found)"
+fi
+if test -z "$LUA_CFLAGS" ; then
+    LUA_CFLAGS="$(pkg-config --cflags lua5.2)"
+fi
+if test -z "$LUA_LDFLAGS" ; then
+    LUA_LDFLAGS="$(pkg-config --libs lua5.2)"
+fi
+if ! test -z "$LUA_PATH" ; then
+    export PATH="$LUA_PATH:$PATH"
+fi
 test -d build-linux || mkdir -m 0700 build-linux
 find build-linux -mindepth 1 -delete
 cd build-linux
 cat ../shellprompt_os_linux.lua ../shellprompt.lua > shellpromptall.lua
 luac -o shellprompt.luac shellpromptall.lua
 lua ../file2h.lua shellprompt.luac shellprompt_lua > shellprompt_lua.h
-LUA="${LUA:-$(pkg-config --cflags --libs lua5.2)}"
-gcc -Wall -Wextra -g -O -I . -o shellprompt ../shellprompt.c ../shellprompt_os_unix.c $LUA
+gcc -Wall -Wextra -g -O -I . $LUA_CFLAGS -o shellprompt ../shellprompt.c ../shellprompt_os_unix.c $LUA_LDFLAGS
