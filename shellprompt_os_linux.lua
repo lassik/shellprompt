861b55f4fcbed8bdb6cde2555b36173ce0a34fee
Lassi Kortela <lassi@lassikortela.net>
2015-01-04 13:48:27 +0200

    On Linux, look for other batteries in addition to BAT1

--- shellprompt_os_linux.lua
+++ shellprompt_os_linux.lua
@@ -12,13 +12,19 @@ function shellprompt_os_getpowerinfo()
   local charging = nil
   local curcharge = nil
   local maxcharge = nil
-  for i, bat in ipairs({"BAT1"}) do
-    local info = contents_or_blank("/proc/acpi/battery/"..bat.."/info")
-    local state = contents_or_blank("/proc/acpi/battery/"..bat.."/state")
-    charging = state:match("charging state:%s*(%a+)")
-    curcharge = state:match("remaining capacity:%s*(%d+)")
-    maxcharge = info:match("last full capacity:%s*(%d+)")
+  local MAXBAT = 5  -- TODO: Pulled this one out of a hat
+  for batnum=0,MAXBAT do
+    local batdir = "/proc/acpi/battery/BAT"..tostring(batnum)
+    local info = contents_or_blank(batdir.."/info")
+    local state = contents_or_blank(batdir.."/state")
+    if info ~= "" and state ~= "" then
+      charging = state:match("charging state:%s*(%a+)")
+      curcharge = state:match("remaining capacity:%s*(%d+)")
+      maxcharge = info:match("last full capacity:%s*(%d+)")
+      break
+    end
   end
+  -- For computers without a battery, fake 100% charge
   curcharge = curcharge or 100
   maxcharge = maxcharge or 100
   local percent = math.max(0, math.min(100, curcharge*100/maxcharge))
